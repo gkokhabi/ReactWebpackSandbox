2f011f87fc4bedd688c1ee23f0dfe3d41e4f15dd
gkokhabi <91689217+gkokhabi@users.noreply.github.com>
2022-02-07 13:43:50 -0500

    webpack react sandbox ready

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
 const Index = () => {
-  return <div>Welcome to React!</div>;
+  return <div>I'm a silly little entry point!</div>;
 };
 ReactDOM.render(<Index />, document.getElementById('app'));
